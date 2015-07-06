281aa7109b7f251337c043c55e211d125fb19be1
Fernando Pardo <firedragon_fernando@hotmail.com>
2015-07-06 17:23:36 -0500

    Cambios menores a exportar.sh
    Se cambiaron los” rm -rfv” a “rm -f” para mayor seguridad a la hora de
    borrar. Asimismo, se cambió el nombre del archivo “borrar_tabla.py” a
    “crear_tablas.py”

--- 1_exportar_sqlite/exportar.sh
+++ 1_exportar_sqlite/exportar.sh
@@ -7,25 +7,26 @@
 # El && sirve para ejecutar una acción si y sólo si la anterior fue exitosa.
 base_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # ${BASH_SOURCE[0]}: nombre del script
-echo $base_dir
+# echo $base_dir
+
 # Directorio dentro de web2py (variable global)
 base_web2py=$(cd "../web2py/web2py.app/Contents/Resources/applications/cliente_web2py/databases" && pwd)
-echo $base_web2py
+#echo $base_web2py
 # Creamos la carpeta bases (en caso de que no exista)
 mkdir ${base_dir%%/}/bases
 
 # Comenzamos borrando las bases en la carpeta bases para tener una sesión nueva
-rm -rfv ${base_dir%%/}/bases/*
+rm -f ${base_dir%%/}/bases/*
 
 # # Exportar la base de datos
 exporta_csv () {
 	echo "Limpiar carpeta databases"
 	# Borrar los contenidos del directorio databases
-	rm -rfv ${base_web2py%%/}/*
+	rm -f ${base_web2py%%/}/*
 
 	# Crear nuevas tablas
 	# -M es para cargar los modelos de la aplicación utilizada.
-	../web2py/web2py.app/Contents/MacOS/web2py -S cliente_web2py -M -R ${base_dir%%/}/scripts_py/borrar_tabla.py
+	../web2py/web2py.app/Contents/MacOS/web2py -S cliente_web2py -M -R ${base_dir%%/}/scripts_py/crear_tablas.py
 
 	# Copiar base de datos sqlite que nos entregaron y pegarla en el cliente
 	echo "reemplazar"
@@ -55,10 +56,10 @@ done
 # Antes de fusionar borrar todo lo del folder databases del fusionador
 base_fusionador=$(cd "../web2py/web2py.app/Contents/Resources/applications/fusionador_sqlite/databases" && pwd)
 
-rm -rfv ${base_fusionador%%/}/*
+rm -f ${base_fusionador%%/}/*
 
 ../web2py/web2py.app/Contents/MacOS/web2py -S fusionador_sqlite -M -R ${base_dir%%/}/scripts_py/fusionar.py -A ${base_dir%%/}/bases
 
 mv ${base_fusionador%%/}/storage.sqlite ${base_dir%%/}/bases/storage.sqlite
 
-rm -rfv ${base_web2py%%/}/*
+rm -f ${base_web2py%%/}/*
