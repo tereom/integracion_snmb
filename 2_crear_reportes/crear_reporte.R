f43c6a4f840e1d2cc70153a7bee516400516dcc9
tereom <teresa.ortiz.mancera@gmail.com>
2015-08-24 15:47:32 -0500

    mejorar reportes repetidos

--- 2_crear_reportes/crear_reporte.R
+++ 2_crear_reportes/crear_reporte.R
@@ -1,11 +1,10 @@
 # Versión de R: 3.2.1 ó superior.
 # corre el rmd que genera reportes de entrega
 # Ejemplo: 
-# > Rscript crear_reporte.R 'FMCN' 'bases_prueba' '\.sqlite'
+# > Rscript crear_reporte.R 'entrega' 'bases_prueba' '\.sqlite'
 # Argumentos:
 # entrega: nombre del directorio donde se guardará el análisis
 # dir_j: ruta de la carpeta donde se buscará la base de datos a revisar
-# pattern_db: regex que identifica las bases de datos a considerar
 # El ejemplo crea:
 # 1 copia de datos: reportes/aaaa_mm_dd_entrega/aaaa_mm_dd_entrega.db
 # 2 reporte pdf: reportes/aaaa_mm_dd_entrega/aaaa_mm_dd_entrega.pdf
@@ -13,11 +12,14 @@
 
 library(rmarkdown)
 
+# apuntamos al pandoc de RStudio
 Sys.setenv(RSTUDIO_PANDOC = '/Applications/RStudio.app/Contents/MacOS/pandoc')
-args <- commandArgs(trailingOnly = TRUE)
 
+# leemos los argumentos de la linea de comandos
+args <- commandArgs(trailingOnly = TRUE)
 nombre_entrega <- args[1]
 dir_j <- args[2]
+
 # si se requiere pattern_db se puede enviar como argumento desde la terminal
 # pattern_db <- args[3]
 pattern_db <- "\\.sqlite$"
@@ -26,12 +28,18 @@ pattern_db <- "\\.sqlite$"
 fecha_reporte <- format(Sys.time(), "%Y_%m_%d")
 entrega <- paste(fecha_reporte, "_", nombre_entrega, sep = "")
 
-output_dir = paste("reportes/", entrega, sep = "")
-output_file = paste(entrega, ".pdf", sep = "")
-render('revision_gral.Rmd', output_file = output_file, output_dir = output_dir)
+# crear estructura de carpetas
+# directorio padre dentro de carpeta reportes
+dir.create("reportes")
+dir_padre <- paste("reportes/", entrega, sep = "")
+dir.create(dir_padre)
 
-output_file = paste(entrega, ".docx", sep = "")
-  render('revision_gral_word.Rmd', output_file = output_file, 
+output_dir = paste("reportes/", entrega, sep = "")
+output_pdf = paste(entrega, ".pdf", sep = "")
+render('revision_gral.Rmd', output_file = output_pdf, output_dir = output_dir)
+
+output_doc = paste(entrega, ".docx", sep = "")
+  render('revision_gral_word.Rmd', output_file = output_doc, 
   output_dir = output_dir)
 
 # revisamos su hay repetidos, y si hace falta creamos reporte y txt
@@ -41,10 +49,11 @@ cgl_reps <- conglomerado_reps %>%
 ids_reps <- cgl_reps$id[duplicated(select(cgl_reps, nombre, fecha_visita))]
 
 if(length(ids_reps) > 0){
-  output_file = paste(entrega, "_rep.pdf", sep = "")
-  output_file = paste(output_dir, "/", entrega, "_rep.txt", sep = "")
-  write.table(ids_reps, file = output_file, quote = FALSE, row.names = FALSE, 
+  # creamos una lista de ids de los conglomerados repetido
+  output_txt = paste(output_dir, "/", entrega, "_rep.txt", sep = "")
+  write.table(ids_reps, file = output_txt, quote = FALSE, row.names = FALSE, 
     col.names = FALSE)
-  render('revision_repetidos.Rmd', output_file = output_file, 
+  output_rep = paste(entrega, "_rep.pdf", sep = "")
+  render('revision_repetidos.Rmd', output_file = output_rep, 
   output_dir = output_dir)
 }
