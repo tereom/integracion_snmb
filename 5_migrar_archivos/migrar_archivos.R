58519ac3cf6c0ea73137a5c146220c88bea6d495
Fernando Pardo <firedragon_fernando@hotmail.com>
2015-11-26 18:04:24 -0600

    Ahora se calcula peso de los archivos migrados en "migrar_archivos.R"
    El script “migrar_archivos.R” ahora genera un csv con el nombre, ruta y
    peso de los archivos emigrados. Asimismo, se revisó el script y parece
    estar listo para migraciones de archivos correspondientes a entregas
    futuras.

--- 5_migrar_archivos/migrar_archivos.R
+++ 5_migrar_archivos/migrar_archivos.R
@@ -777,7 +777,7 @@ Archivo_conteo_ave_ruta <- Archivo_conteo_ave_info %>%
     salida = paste(entrega, "/", nombre_anio_mes, "/otros/", nuevo_nombre, sep = "")
   ) %>%
   select(
-    nombre_anio_mes,
+    nombre_anio_mes, 
     entrada = archivo,
     salida,
     archivo_nombre_original,
@@ -863,14 +863,14 @@ terminaciones <- Archivo_ruta %>%
 tipos_archivo <- unique(terminaciones$terminacion)
 
 # Enlistando todos los archivos en el directorio origen y guardándlos en un df:
-# lista_archivos_j <- list.files(path = dir_j, recursive = TRUE, full.names = TRUE)
+lista_archivos_j <- list.files(path = dir_j, recursive = TRUE, full.names = TRUE)
 
 # Escribiendo archivo
 # write.table(lista_archivos_j, file = "2015_08_11_archivos_snmb_cluster.csv",
 #    sep=",", row.names = FALSE)
 
 # Leer archivo
-lista_archivos_j <- read.csv("aux/2015_08_11_archivos_snmb_cluster.csv", stringsAsFactors = FALSE)$x
+#lista_archivos_j <- read.csv("aux/2015_08_11_archivos_snmb_cluster.csv", stringsAsFactors = FALSE)$x
 
 ################################################################################
 ### funciones para generar nombres de archivos incluso para los muy largos ###
@@ -1153,3 +1153,21 @@ Archivos_no_registrados_origen_destino <- Archivo_origen %>%
 # puesto que son archivos no registrados en la base de datos.)
 resultados_nr <- apply(Archivos_no_registrados_origen_destino, 1,
   function(x) file.copy(x['ruta_origen'], x['ruta_destino'], overwrite = FALSE))
+
+#Calculando el tamaño de los archivos migrados (en bytes):
+files_media <- Rutas_origen_destino %>%
+  filter(resultados) %>%
+  .$ruta_destino
+size_media <- file.size(files_media)
+
+#Creando data frame con los tamaños de los archivos
+tamano_archivos <- data.frame(
+  nombre = splitPathFile(files_media)[[3]],
+  ruta = files_media,
+  tamano = size_media)
+
+write.csv(tamano_archivos, file = paste0(
+  dir_padre, "/", nombre_base, "_archivados_migrados_registrados_bd.csv"),
+  row.names = FALSE)
+
+
